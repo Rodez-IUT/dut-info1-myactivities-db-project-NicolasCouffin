1e28e2a4686fc09e73511e67b97dab7775fe6aec
Nicolas Couffin <nicolas.couffin@iut-rodez.fr>
2020-04-29 14:00:56 +0200

    fix #8 vue P3-3 vue reservations

--- P3-3-vues_reservations.sql
+++ P3-3-vues_reservations.sql
@@ -6,5 +6,5 @@ CREATE VIEW gds.vue_reservations AS
 		gds.reservation.date_fin as date_de_fin
 	FROM gds.reservation
 	JOIN gds.salle
-	ON id_salle = gds.salle.id
+	ON salle_id = gds.salle.id
 	ORDER BY nom_salle, date_debut, date_debut DESC;
